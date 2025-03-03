4403f67dc134b9de768e590c8666b2d9e256d8f0
maryembdira <maryem.bdirafilali@esprit.tn>
2025-03-04 00:51:53 +0100

    update

---
+++ mainwindow.h
@@ -0,0 +1,32 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+#include <QPixmap>
+#include <QIcon>
+#include <QMainWindow>
+#include <QSqlDatabase>
+#include <QSqlQuery>
+#include <QMessageBox>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class MainWindow;
+}
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_pushButton_clicked();
+
+private:
+    Ui::MainWindow *ui;
+     void remplirTableau();
+};
+
+#endif // MAINWINDOW_H
